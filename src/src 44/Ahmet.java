5d5b0358bbefce665b80f8e7a1ca4e1f36bf71f5
nldn24 <ahmetnaldan24@hotmail.com>
2025-09-23 11:05:41 +0300

    Değişiklik Yaptım1

--- src/src 44/Ahmet.java
+++ src/src 44/Ahmet.java
@@ -1,6 +1,6 @@
 public class Ahmet {
     public static void main(String[] args)
     {
-        System.out.println("Değişiklik yaptım");
+        System.out.println("Değişiklikler Geldi mi");
     }
 }
