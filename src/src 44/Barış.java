5d81d68a20a885bf02d4978850cc8f6bb51a5214
BRC1234210 <askbarrcan41@gmail.com>
2025-09-23 11:33:10 +0300

    created

--- src/src 44/Barış.java
+++
@@ -1,9 +0,0 @@
-public class Barış {
-    public static void main(String[] args) {
-        System.out.println("barış cansız");
-        System.out.println("güncelleme yapıldı ");
-
-        System.out.println("Zilha Kalac");
-        System.out.println("Proje bitsin artık!! yetti gari be..");
-    }
-}
--- src/src 44/Modeller/ahmet.java
+++
@@ -1,7 +0,0 @@
-package Modeller;
-
-public class ahmet {
-    public static void main(String[] args) {
-        System.out.println("*****");
-    }
-}
